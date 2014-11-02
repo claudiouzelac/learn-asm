65e0080d890b7a5588b7e5d02f6ca1dbd98b622d
Nicholas Mazzuca <npmazzuca@gmail.com>
2014-11-02 13:24:34 -0800

    Chapter 1: Complete

---
+++ src/exit.s
@@ -0,0 +1,17 @@
+;   exit
+;
+;   Executes exit system call
+;
+;   Input:
+;
+;   Output: 
+;       Exit status: 0
+;
+
+segment .text
+global _start
+_start:
+    mov eax, 60  ; 60 is the exit syscall
+    mov edi, 0   ; Return 5
+    syscall      ; Kernel interrupt
+    end
