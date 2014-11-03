160ee3ab399116c6e2f666786cc648cb9b2e62b3
Nicholas Mazzuca <npmazzuca@gmail.com>
2014-11-02 16:01:06 -0800

    Chapter 2, 3

---
+++ src/Ch3/exc1.s
@@ -0,0 +1,6 @@
+section .data
+a times 10 dq 0
+c db "Hello world", 0
+
+section .bss
+b resw 5
---
+++ src/Ch3/mem.s
@@ -0,0 +1,22 @@
+segment .data
+a dd 4
+b dd 4.4
+c times 10 dd 0
+d dw 1, 2
+e db 0xFB
+f db "hello world", 0
+
+segment .bss
+g resd 1
+h resd 10
+i resb 100
+
+segment .text
+global main
+main:
+    push rbp  ; set up stack frame
+    mov rbp, rsp ; rbp points to the stack frame
+    sub rsp, 16  ; room for local variables
+    xor eax, eax ; rax = 0 for return
+    leave        ; undo stack frame changes
+    ret
